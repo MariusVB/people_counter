0d545ab53611431c2715c7652533c10691eece14
Marius Venø Bendsen <mariusbendsen@gmail.com>
2020-10-08 17:09:00 +0200

    Fix

--- count-repo/people_counter.py
+++ count-repo/people_counter.py
@@ -43,6 +43,8 @@ ap.add_argument("-sc", "--show-cam", type=int, default=1,
 
 args = vars(ap.parse_args())
 
+print(args)
+
 # initialize the list of class labels MobileNet SSD was trained to
 # detect
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
@@ -275,7 +277,8 @@ while True:
 	if writer is not None:
 		writer.write(frame)
 
-	if args.get('show-cam', 1) == 1:
+	if args.get('show_cam', 1) == 1:
+		print(args.get('show_cam'))
 		# show the output frame
 		cv2.imshow("Frame", frame)
 		key = cv2.waitKey(1) & 0xFF
